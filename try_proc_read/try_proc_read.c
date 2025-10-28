fda19bae89bdd223c615008fc894f6bf8328c452
Thomas Sabu <sansuisa@gmail.com>
2025-10-28 23:27:12 +0530

    proc read added

--- try_proc_read/try_proc_read.c
+++ try_proc_read/try_proc_read.c
@@ -10,15 +10,39 @@ MODULE_VERSION("1.1.0");
 
 static struct proc_dir_entry *hello_driver_proc_entry;
 
-struct proc_ops hello_driver_proc_ops ={ 
-					
-					};
+static ssize_t hello_driver_read (struct file * file_pointer,
+	               	char *user_space_buffer, 
+			size_t count, 
+			loff_t* offset) 
+{
+	char msg[] ="did a proc read. Acknowledged\n";
+	ssize_t length =strlen(msg);
+	int result;
+	
+	printk(" hello_driver proc read_function \n");
+
+	if(*offset >= length)
+	{
+		return 0;
+	}
+
+	result = copy_to_user (user_space_buffer, msg, length);
+
+	*offset += length;
+	return length;
+}
+
+
+struct proc_ops hello_driver_proc_ops =
+{ 
+	.proc_read =hello_driver_read					
+};
 
 static int __init hello_driver_init(void)
 {
 	printk("hello_driver loading to kernel started \n ");
 
-	hello_driver_proc_entry = proc_create("hello_driver",
+	hello_driver_proc_entry = proc_create("hello_driver_proc_read",
 
                			0, 
 				NULL, 
